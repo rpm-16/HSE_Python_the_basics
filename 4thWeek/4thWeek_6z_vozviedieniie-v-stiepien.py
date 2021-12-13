1f78dc282a1a22325324b59b938f31329b895728
rpm-16 <rpm-16@mail.ru>
2021-12-14 00:56:56 +0300

    4th week 7th tack done. Im going sleep

--- 4thWeek/4thWeek_6z_vozviedieniie-v-stiepien.py
+++ 4thWeek/4thWeek_6z_vozviedieniie-v-stiepien.py
@@ -7,8 +7,8 @@
 def power(a, n):
     if n == 0:
         return 1
-    return a*power(a, n-1)
+    return a * power(a, n-1)
 
 
-a, n = int(input()), int(input())
+a, n = float(input()), float(input())
 print(power(a, n))
